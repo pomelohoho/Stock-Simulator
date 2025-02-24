1b30c472e90dd37cb73b14443a11a4cf12c0cac2
pomelohoho <52699926+pomelohoho@users.noreply.github.com>
2025-02-24 15:08:39 -0600

    Delete model-portfolio directory

--- model-portfolio/src/data/albums.js
+++
@@ -1,9 +0,0 @@
-export const albums = [
-    {
-      id: 1,
-      title: "Summer Collection",
-      thumbnail: "/images/summer-thumb.jpg",
-      photos: ["/images/summer1.jpg", "/images/summer2.jpg"]
-    },
-    // Add more albums...
-  ];
